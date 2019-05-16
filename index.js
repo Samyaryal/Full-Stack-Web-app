d625dbbf1183fdc4c99093f13ad55a65d7ba3db4
Samikshya Aryal <samikshya@edu92-22.turkuamk.fi>
2019-05-16 16:16:37 +0300

    capital s

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
-require('./services/passport');
+require('./Services/passport');
 
 mongoose.connect(keys.mongoURI);
 
