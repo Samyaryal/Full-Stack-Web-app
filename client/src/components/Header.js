29950624f3481fa95d869fb7147ee7d4cf8c73a8
Samikshya <samikshyaaryal5@gmail.com>
2019-06-08 20:04:53 +0300

    Link Tags

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -17,10 +18,13 @@ class Header extends Component {
     render() {
         return (
             <nav>
-                <div className="navwrapper">
-                    <a className="left-brand-logo">
-                       Emaily
-                    </a>
+                <div className="nav-wrapper">
+                    <Link 
+                        to={this.props.auth ? '/surveys' : '/'} 
+                        className="left-brand-logo"
+                    >
+                        Emaily
+                    </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
