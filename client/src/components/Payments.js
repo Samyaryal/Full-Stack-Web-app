fd4f0ed81bb0aa843dd1031fb6307b334a89e7b9
Samikshya <samikshyaaryal5@gmail.com>
2019-06-11 09:59:43 +0300

    creating and finialize charge object

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import { connet } from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class Payments extends Component {
@@ -10,7 +10,7 @@ class Payments extends Component {
                 name="Emaily"
                 description="$5 for 5 email credits "
                 amount={500}
-                token={token => this.props.handeToken(token)}
+                token={token => this.props.handleToken(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
             >
                 <button className="btn">
