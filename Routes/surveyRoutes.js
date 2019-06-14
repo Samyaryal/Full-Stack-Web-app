780b55647a97f17b38937223d092fc038ed470cf
Samikshya <samikshyaaryal5@gmail.com>
2019-06-14 19:23:47 +0300

    update

--- Routes/surveyRoutes.js
+++ Routes/surveyRoutes.js
@@ -9,6 +9,7 @@ const Survey = mongoose.model('surveys');
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         const { title, subject, body, recipients} = req.body;
+        
 
         const survey = new Survey ({
             title, 
