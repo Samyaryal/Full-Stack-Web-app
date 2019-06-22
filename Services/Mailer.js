f0b907e869730eaa04669131505cfb4af3c720b6
Samikshya <samikshyaaryal5@gmail.com>
2019-06-22 12:31:29 +0300

    updated

--- Services/Mailer.js
+++ Services/Mailer.js
@@ -22,6 +22,7 @@ class Mailer extends helper.Mail{
 
         });
     }
+    
     addClickTracking() {
         const trackingSettings = new helper.TrackingSettings();
         const clickTracking = new helper.ClickTracking(true, true);
