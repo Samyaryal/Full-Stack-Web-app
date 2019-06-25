f6b9382be3ea1052d553c979f039fc22d2614099
Samikshya <samikshyaaryal5@gmail.com>
2019-06-25 11:46:37 +0300

    Redirect on submit

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,9 +3,10 @@ import _ from 'lodash';
 import React from 'react'; 
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom'; //withrouter  le history pass garxa.  
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => { //history vanne object add garyo
     const reviewFields = _.map(formFields, ({ name, label }) => {
         return (
             <div key={name}>
@@ -20,7 +21,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
             {reviewFields}
             <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>Back</button>
             <button 
-                onClick={()=>submitSurvey(formValues)}
+                onClick={()=>submitSurvey(formValues, history)}
                 className="green  btn-flat right"
             >
                 Send Survey
@@ -34,4 +35,5 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions) (SurveyFormReview);
+export default connect(mapStateToProps, actions) (withRouter(SurveyFormReview)); // withRouter as an normal fuction that takes component 
+                                                                                // as an argument
