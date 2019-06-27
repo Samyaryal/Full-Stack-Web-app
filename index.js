be5e11fc40e1fdb94c1d2041c89baffe3d5e5977
Samikshya <samikshyaaryal5@gmail.com>
2019-06-27 12:01:00 +0300

    FETCHING SURVEYS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('./models/Survey');
 require('./Services/passport');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI ,{ useNewUrlParser: true });
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
