485b20930572c2f8c442dc650100e426cbda263e
Samikshya <samikshyaaryal5@gmail.com>
2019-07-16 13:44:19 +0300

    updated commit

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SurveyList from './surveys/SurveyList';
 
+
 const Dashboard = () => {
     return (
         <div>
--- client/src/reducers/surveysReducer.js
+++ client/src/reducers/surveysReducer.js
@@ -1,6 +1,5 @@
 import { FETCH_SURVEYS } from '../actions/types';
 
-
 export default function(state = [], action) {
     switch (action.type) {
         case FETCH_SURVEYS:
